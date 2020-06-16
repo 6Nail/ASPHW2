50e0e033e20c51dfb0a94bd23c5ab3fccb2c256c
Nail <55398701+6Nail@users.noreply.github.com>
2020-06-16 19:28:46 +0600

    Add files via upload

---
+++ ASP_HW_02/WebApplication3/Models/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Room { get; set; }
+        public List<Student> Students { get; set; }
+        public string CourseName { get; set; }
+    }
+}
